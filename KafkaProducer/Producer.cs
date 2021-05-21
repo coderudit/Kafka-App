81a97bf293da8f25a5601a6018fd3f24c7db435f
gitName <a@b.c>
2021-05-21 11:48:41 +0530

    Add project files.

---
+++ KafkaProducer/Producer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+
+namespace KafkaProducer
+{
+    class Producer
+    {
+        private static readonly ProducerConfig config = new ProducerConfig
+        { BootstrapServers = "localhost:9092" };
+        private static string topic = "f_topic";
+
+        public async Task SendMessagesToCluster()
+        {
+            string key = "1";
+            string value = "mess";
+            using (var producer = new ProducerBuilder<string, string>(config).Build())
+            {
+                try
+                {
+                    await producer.ProduceAsync(topic,
+                        new Message<string, string> { Key = key, Value = value });
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
---
+++ KafkaProducer/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace KafkaProducer
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var producer = new Producer();
+            await producer.SendMessagesToCluster();
+        }
+    }
+}
